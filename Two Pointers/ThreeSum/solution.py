1b0eae529002a70ca64d0e0c592acd7b9304fad1
TayKaiJun <tehohno@gmail.com>
2023-07-30 00:18:38 +0800

    fixed mistake in ThreeSum problem

--- Two Pointers/ThreeSum/solution.py
+++ Two Pointers/ThreeSum/solution.py
@@ -5,7 +5,7 @@ def solution(nums):
     for i in range(len(nums)):
         if nums[i] > 0:
             break
-        if 0 < i < len(nums)-1 and nums[i] == nums[i-1]:
+        if 0 < i < len(nums) - 1 and nums[i] == nums[i - 1]:
             continue
 
         j = i + 1
@@ -16,6 +16,9 @@ def solution(nums):
                 answer.append([nums[i], nums[j], nums[k]])
                 j += 1
                 k -= 1
+                while (0 < j < len(nums) - 1  and nums[j] == nums[j-1]) and (0 < k < len(nums) - 1 and nums[k] == nums[k+1]):
+                    j += 1
+                    k -= 1
             elif result < 0:
                 j += 1
             else:
@@ -25,6 +28,7 @@ def solution(nums):
 
 if __name__ == "__main__":
     print(solution([-1, 0, 1, 2, -1, -4]))  # [[-1,-1,2],[-1,0,1]]
-    print(solution([4,-1, -3, -3, 0, 0, 0, 0, 2, 3, 3, 6]))
-    print(solution([0,1,1]))            # []
-    print(solution([0,0,0]))            # [0,0,0]
+    print(solution([4, -1, -3, -3, 0, 0, 0, 0, 2, 3, 3, 6]))
+    print(solution([0, 1, 1]))  # []
+    print(solution([0, 0, 0]))  # [0,0,0]
+    print(solution([-2, 2, 2, 0, 0, 2, 0, 0]))
