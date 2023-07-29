b42b55533e2dc64b1ccbd123d2957a207d54beea
TayKaiJun <tehohno@gmail.com>
2023-07-29 20:59:07 +0800

    Improved H-index solution from O(nlogn) to O(n)

--- Two Pointers/ContainerWithMostWater/solution.py
+++ Two Pointers/ContainerWithMostWater/solution.py
@@ -1,27 +1,20 @@
 def solution(height):
     n = len(height)
-    left = n // 2 - 1
-    right = n // 2
-
+    left = 0
+    right = n - 1
     container = 0
-    while left > 1 or right < n:
+    while left < right:
         current = min(height[left], height[right]) * (right - left)
-        if current > container:
-            container = current
+        container = max(container, current)
         if height[left] < height[right]:
-            if left > 0:
-                left -= 1
-            else:
-                right += 1
+            left += 1
         else:
-            if right < n:
-                right += 1
-            else:
-                left -= 1
-
+            right -= 1
     return container
 
+
 if __name__ == "__main__":
-    print(solution([1,8,6,2,5,4,8,3,7])) # 49
-    print(solution([8, 7, 3, 3, 7, 8])) # 40
-    print(solution([1,1])) # 1
+    print(solution([1,8,6,2,5,4,8,3,7]))    # 49
+    print(solution([8, 7, 3, 3, 7, 8]))     # 40
+    print(solution([1,1]))                  # 1
+    print(solution([1,2,4,3]))              # 4
