e326812b8351000d2077787a8296b22ca512cf28
TayKaiJun <tehohno@gmail.com>
2023-09-01 01:43:15 +0800

    proper two pointer solution for palindrome

--- Two Pointers/ValidPalindrome/solution.py
+++ Two Pointers/ValidPalindrome/solution.py
@@ -1,14 +1,33 @@
 import re
 
 def solution(s):
-    cleanString = re.sub("[^a-zA-Z\d]", "", s).lower()
-    n = len(cleanString)
-    if n == 1:
-        return False
+    # cleanString = re.sub("[^a-zA-Z\d]", "", s).lower()
+    # n = len(cleanString)
+    # if n == 1:
+    #     return False
 
-    for i in range(n//2):
-        if not cleanString[i] == cleanString[n-1-i]:
+    # for i in range(n//2):
+    #     if not cleanString[i] == cleanString[n-1-i]:
+    #         return False
+    # return True
+    
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+            continue
+        if not s[right].isalnum():
+            right -= 1
+            continue
+        
+        if not s[left].lower() == s[right].lower():
             return False
+        else:
+            left += 1
+            right -= 1
+    
     return True
 
 
@@ -16,4 +35,4 @@ if __name__ == "__main__":
     print(solution("A man, a plan, a canal: Panama")) # true
     print(solution("race a car")) # false
     print(solution(" ")) # true
-    print(solution("0P")) # true
+    print(solution("0P")) # false
